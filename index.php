9355b6e00443b318314f04eb81ab719454655e9b
Gaspar <gaspar.herman95@gmail.com>
2020-10-20 17:36:28 +0200

    Agregada prueba de estilo

--- index.php
+++ index.php
@@ -8,13 +8,41 @@
         <script src="/js/import_menu.js"></script>
     </head>
     
-    <body onload="return importHTML();">
-       <div id="loadMenu"></div>
-
+    
+    <body>
+       <div id="loadMenu">
+       
+       <nav>
+       <div id="productIndex">
        <?php
        require __DIR__.'/controller/list_products.php';
+       
        $categories = getCategories();
-       echo $categories[0]['nombre'];
+       echo '<ul class="navigationMenu">';
+       foreach ($categories as $category) {
+          
+           $element = $category['nombre'];
+            echo '<li><a href="/index.html">'.$element.'</a></li>';
+       }
+       echo '</ul>';
        ?>
+       </div>
+       </nav>
+       </div>
+       <nav>
+<div id="productIndex">
+    <ul class="navigationMenu">
+        <li><a href="/index.html">Inicio</a></li>
+        <li><a href="/menuElements/cerveza.html">Cervezas</a></li>
+        <li><a href="/menuElements/vinos.html">Vinos</a></li>
+        <li><a href="/menuElements/whisky.html">Whisky</a> </li>
+        <li><a href="/menuElements/ron.html">Ron</a></li>
+        <li><a href="/menuElements/vodka.html">Vodka</a></li>
+        <li><a href="/menuElements/cocteles.html">Cocteles</a></li>
+        <li><a href="/menuElements/ginebras.html">Ginebras</a></li>
+        <li><a href="/menuElements/cavas.html">Cavas</a></li>
+    </ul>
+</div>
+</nav>
     </body>
 </html>
