78a731856c617c1ef0aedb49422087c85e6520e0
Gaspar Hermán <gaspar@GHLinux.home>
2020-10-23 20:51:44 +0200

    Agregada variable de entorno para poder distinguir el host

--- model/Connection.php
+++ model/Connection.php
@@ -4,12 +4,13 @@ class Connection {
 private $databaseName = 'tdiwb8';
 private $databaseUserName = 'tdiw-b8';
 private $databasePassword;
-private $databaseHost = 'db';
+private $databaseHost;
 private $databaseConnection;
 
     public function __construct()
     {
         $this->setPassword();
+        $this->setHost();
         try {
             $this->databaseConnection = new PDO('mysql:host=' . $this->databaseHost . ';dbname=' . $this->databaseName . ';port=3306;charset=utf8mb4', $this->databaseUserName, $this->databasePassword);
             $this->databaseConnection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
@@ -46,6 +47,12 @@ private $databaseConnection;
         $foundedCategory = $this->doQuery($typeOfCategoryQuery);
 
         return array($foundedCategory[0]["nombre"],$productsInCategory);
+    }
+
+    private function setHost()
+    {
+        $this->databasePassword = getenv('MYSQL_PASSWORD') == FALSE ? 'RwJv1j9D' : getenv('MYSQL_PASSWORD');
+        $this->databaseHost = getenv('DATABASE_HOST') == FALSE ? 'localhost' : getenv('DATABASE_HOST');
     }
 }
 ?>
