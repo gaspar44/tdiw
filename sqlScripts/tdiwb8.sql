c8197c27317ec2cf282308d8269f65710e3b414f
Gaspar <gaspar.herman95@gmail.com>
2020-12-05 22:30:57 +0100

    Backup de la base de datos

--- sqlScripts/tdiwb8.sql
+++ sqlScripts/tdiwb8.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb4+deb9u2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Nov 16, 2020 at 06:36 PM
--- Server version: 10.1.47-MariaDB-0+deb9u1
--- PHP Version: 7.3.7-1+0~20190710.40+debian9~1.gbp032aec
+-- Host: db
+-- Generation Time: Dec 05, 2020 at 09:30 PM
+-- Server version: 10.1.38-MariaDB-1~bionic
+-- PHP Version: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -99,7 +100,7 @@ INSERT INTO `producto` (`id`, `nombre`, `precio`, `descripcion`, `ruta`, `catego
 (3, 'Chardonnay 2018', 5.51, 'De nuestra finca “El Cerezo”, elaboramos un Chardonnay Joven con las características de un clima fresco y altitud elevada. Tiene un color brillante amarillo, fresco y persistente, con notas tropicales al inicio que van tornadose cítricas al final del paso por boca. ', '/view/resources/pictures/Chardonnay2018.png', 2),
 (5, 'Chivas Regal XV Gold', 42.9, 'Chivas Regal XV es un whisky escocés de 15 años con un sabor único y refinado, afrutado y aterciopelado. El secreto de su extraordinario sabor es el selectivo proceso de acabado en sus barricas usadas anteriormente para madurar Coñacs de Grande Champagne.', '/view/resources/pictures/ChivasRegal.jpg', 3),
 (6, 'Ron Zacapa Centenario XO', 142, 'Su nobleza excepcional se lugra gracias a un añejamiento de hasta 25 años a 2.300 metros de altitud. El resultado es un equilibrio grandioso entre complejos aromas y sabores que le hace ser considerado por muchos \'el Cognac de los rones\'. 70cl ', '/view/resources/pictures/RonZacapa.jpg', 5),
-(7, 'Vodka RIP singular Madrid', 28.6, 'Elaborado de forma artesanal 100% natural y sin filtrar.\r\nRIP es un vodka puro, cristalino, de gran untuosidad y lleno de personalidad y matices provenientes de la uva del país. El destilado durante horas en cobre y lotes reducidos le da la máxima suavidad y redondez en boca. 50cl 40%', '/view/resources/pictures/VodkaRipSingularMafrid.png', 6),
+(7, 'Vodka RIP singular Madrid', 28.6, 'Elaborado de forma artesanal 100% natural y sin filtrar.\r\nRIP es un vodka puro, cristalino, de gran untuosidad y lleno de personalidad y matices provenientes de la uva del país. El destilado durante horas en cobre y lotes reducidos le da la máxima suavidad y redondez en boca. 50cl 40%', '/view/resources/pictures/VodkaRipSingularMafrid.jpg', 6),
 (8, 'Castell Sant Antoni', 13.95, 'De color amarillo pálido con reflejos verdosos.\r\n\r\nBurbuja fina y abundante, con formación de rosarios de ascendencia constante.\r\n\r\nEn nariz es fresco y agradable. Destacan aromas de fruta blanca (manzana verde, melocotón, albaricoque), además de ciertos aromas de flores blancas, notas cítricas y lías finas (pan de molde).\r\n\r\nEn boca la entrada es fresca y viva, con una muy buena acidez y carbónico bien integrado.\r\n\r\nEl paso en boca es ágil y la sensación final es agradable y limpia. 75cl 12%Vol', '/view/resources/pictures/CastellSantAntoni.jpg', 4);
 
 -- --------------------------------------------------------
@@ -123,7 +124,7 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nombre`, `mail`, `password`, `direccion`, `poblacion`, `cp`) VALUES
-(1, 'Gaspar', 'gaspar.herman95@gmail.com', '$2y$10$OOlM3z7QT/2umXM034VO0OSqOuTiYOsPwOKHocIwwx.71sHemqRwK', '1234', 'Sant Cugat', 8912);
+(2, 'Gaspar Hermán', 'gaspar.herman95@gmail.com', '$2y$10$.wbIEt76jhDXIyBkDP6.v.DHora5.9OEPU/Lr9ALswMVmKap0E4Gu', '123', 'Sant Cugat', 8912);
 
 --
 -- Indexes for dumped tables
@@ -171,27 +172,32 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT for table `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- AUTO_INCREMENT for table `comanda`
 --
 ALTER TABLE `comanda`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `linea_comanda`
 --
 ALTER TABLE `linea_comanda`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `producto`
 --
 ALTER TABLE `producto`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- Constraints for dumped tables
 --
@@ -214,6 +220,7 @@ ALTER TABLE `linea_comanda`
 --
 ALTER TABLE `producto`
   ADD CONSTRAINT `fk_producto_categoria_id` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
