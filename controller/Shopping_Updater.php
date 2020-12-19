a309ad5b64fc726e99e46d751a50c2915e3cd4e3
Gaspar <gaspar.herman95@gmail.com>
2020-12-19 21:46:21 +0100

    #8 - Arreglado errores sobre la página del entorno final

--- controller/Shopping_Updater.php
+++ controller/Shopping_Updater.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../model/Product.php';
 require_once __DIR__ . '/../model/Shopping_Car.php';
 
+
 if (!isset($_SESSION["shoppingCar"])) {
     http_response_code(401);
     return;
--- controller/User_Profile.php
+++ controller/User_Profile.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if(!isset($_SESSION)) {
+    session_start();
+}
 
 if (!isset($_SESSION["shoppingCar"])) {
     http_response_code(401);
