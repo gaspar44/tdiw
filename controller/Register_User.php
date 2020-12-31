3e5efbc2ff2337858592d3989023245f02152f7f
Gaspar <gaspar.herman95@gmail.com>
2020-12-31 14:42:42 +0100

    Arregladas verificaciones en servidor

--- controller/Register_User.php
+++ controller/Register_User.php
@@ -2,12 +2,35 @@
 require_once __DIR__. "/../model/User_Factory.php";
 require_once __DIR__ . "/../model/Message.php";
 
-$userName = $_POST["mail"];
-$address = $_POST["direccion"];
-$poblation = $_POST["poblacion"];
-$postalCode = $_POST["codigo_postal"];
-$userRealNames = $_POST["nombres"];
-$password = $_POST["password"];
+$userName = $_POST["mail"] ?? null;
+$address = $_POST["direccion"] ?? null;
+$poblation = $_POST["poblacion"] ?? null;
+$postalCode = $_POST["codigo_postal"] ?? null;
+$userRealNames = $_POST["nombres"] ?? null;
+$password = $_POST["password"] ?? null;
+$repitPassword = $_POST["newPassword"] ?? null;
+
+if (is_null($userName) || is_null($address) || is_null($poblation) || is_null($postalCode) || is_null($userRealNames) || is_null($password) || is_null($repitPassword)) {
+    http_response_code(400);
+    die();
+}
+
+if (filter_var($userName,FILTER_VALIDATE_EMAIL) == false) {
+    echo "error en corrreo";
+    require_once __DIR__ . '/../view/RegistroUsuario.html';
+    return;
+}
+
+if (!preg_match('/\d{5}/',$postalCode)) {
+    require_once __DIR__ . '/../view/RegistroUsuario.html';
+    return;
+}
+
+if ($repitPassword != $password) {
+    echo "contraseñas no coinciden";
+    require_once  __DIR__ . '/../view/RegistroUsuario.html';
+    return;
+}
 
 $factory = new UserFactory();
 $user = $factory->getUser($userName,$password,$address,$poblation,$postalCode,$userRealNames);
